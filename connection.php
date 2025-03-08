05d433593894156f81f71eb86c041e561b91ecd0
BinaRRR <binarbeta@gmail.com>
2025-03-09 00:53:07 +0100

    CS2 licenses

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $hostname = "145.239.236.240";
-    $username = "srv59257";
-    $dbpassword = "GvnQFAMTp0";
-    $database = $username;
-?>
-
--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', e => {
-    var body = document.querySelector('body');
-    body.classList.remove("preload");
-});
